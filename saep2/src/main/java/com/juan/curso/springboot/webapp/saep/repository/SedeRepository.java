3aeb6c5532a6547bdd914d7c39bd62bd3dccc81b
Andres <andresitowow123@gmail.com>
2025-06-16 08:45:22 -0500

    ....

--- saep2/src/main/java/com/juan/curso/springboot/webapp/saep/repository/SedeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.juan.curso.springboot.webapp.saep.repository;
-
-import com.juan.curso.springboot.webapp.saep.model.Sede;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SedeRepository extends JpaRepository<Sede, Long> {
-
-}
